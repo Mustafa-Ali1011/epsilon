d5dd54eec77a5652cfc24e0af263493e881163e2
Mustafa  Ali <79056138+Mustafa-Ali1011@users.noreply.github.com>
2021-03-23 18:47:30 +0200

    Delete Ass_6 directory

--- Ass_6/my_packages/Transformers/StringTransformer.py
+++
@@ -1,8 +0,0 @@
-def string_reverse():
-     string=input("enter any string :")
-     print(string[::-1])
-
-def string_captalize():
-     string=input("enter any string :")
-     print(string.capitalize())
-     
